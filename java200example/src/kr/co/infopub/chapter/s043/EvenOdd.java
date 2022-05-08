e4b94ceeba33496da50b0a61cbb62f80487bb2d9
leesw1347 <leesw1347@gmail.com>
2022-05-09 02:28:07 +0900

    43. while을 이용하여 1이 될 때까지 나누고 곱하기

---
+++ java200example/src/kr/co/infopub/chapter/s043/EvenOdd.java
@@ -0,0 +1,20 @@
+package kr.co.infopub.chapter.s043;
+
+public class EvenOdd {
+    public static void showOddnEvenw(int n) {
+        int temp = n;
+        while (temp != 1) {
+            if (temp % 2 == 1) { // 홀수
+                temp = temp * 3 + 1;
+            } else {
+                temp /= 2;
+            }
+            System.out.println("[" + temp + "]");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        showOddnEvenw(122);
+    }
+}
