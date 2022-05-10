8199272fabfa469aa07af6dc9ab9ec99d13f7444
leesw1347 <leesw1347@gmail.com>
2022-05-10 10:33:01 +0900

    58.for와 if를 이용하여 홀수에 대한 제곱의 합 구하기

---
+++ java200example/src/kr/co/infopub/chapter/s058/ForCondition2.java
@@ -0,0 +1,14 @@
+package kr.co.infopub.chapter.s058;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForCondition2 {
+    public static void main(String[] args) {
+        List<Integer> ilist = new ArrayList<>();
+        int s = 0;
+        for (int i = 0; i <= 10; i++) {
+            ilist.add(i);
+        }
+    }
+}
