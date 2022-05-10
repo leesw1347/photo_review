d6067bdacaaca12df0d41c5d88b31b0087586b91
leesw1347 <leesw1347@gmail.com>
2022-05-10 10:54:08 +0900

    58.for와 if를 이용하여 홀수에 대한 제곱의 합 구하기

--- java200example/src/kr/co/infopub/chapter/s058/ForCondition2.java
+++ java200example/src/kr/co/infopub/chapter/s058/ForCondition2.java
@@ -10,5 +10,14 @@ public class ForCondition2 {
         for (int i = 0; i <= 10; i++) {
             ilist.add(i);
         }
+
+        for (int m : ilist) {
+            if (m % 2 == 1) {
+                s += m * m;
+                System.out.printf("%d\t", m * m);
+            }
+        }
+        System.out.println();
+        System.out.println("1~10 사이의 홀수에 대한 제곱합 : " + String.valueOf(s));
     }
 }
