f27d55c890744e0d72512eea1daa395eb9d2f050
leesw1347 <leesw1347@gmail.com>
2022-05-10 10:32:10 +0900

    57. foreach와 lambda 형식을 이용하여 List 출력하기

--- java200example/src/kr/co/infopub/chapter/s057/ForeachLambdaLotto.java
+++ java200example/src/kr/co/infopub/chapter/s057/ForeachLambdaLotto.java
@@ -1,9 +1,22 @@
 package kr.co.infopub.chapter.s057;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ForeachLambdaLotto {
     public static void main(String[] args) {
-        List<Integer> mmlist
+        List<Integer> mmlist = Arrays.asList(1, 6, 16, 22, 23, 33);
+        mmlist.forEach(
+                m -> {
+                    System.out.print(String.valueOf(m) + " ");
+                }
+        );
+        System.out.println();
+        Consumer<Integer> consumer = (Integer m) -> {
+            System.out.printf(m + "\t");
+        };
+        mmlist.forEach(consumer);
+        System.out.println();
     }
 }
