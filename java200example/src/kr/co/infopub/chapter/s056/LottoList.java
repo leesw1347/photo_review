670444e7f9f2fae75626cd8c569fbf368b34a32d
leesw1347 <leesw1347@gmail.com>
2022-05-10 10:18:06 +0900

    56. 배열보다 편리한 List 사용하기

---
+++ java200example/src/kr/co/infopub/chapter/s056/LottoList.java
@@ -0,0 +1,14 @@
+package kr.co.infopub.chapter.s056;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoList {
+    public static void main(String[] args) {
+        List<Integer> ilist = new ArrayList<>();
+        // 넣기
+        for (int i = 0; i <= 10; i++) {
+            ilist.add(i);
+        }
+    }
+}
