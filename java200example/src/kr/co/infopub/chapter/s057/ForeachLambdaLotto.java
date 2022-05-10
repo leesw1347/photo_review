c8bf656752e7dda7d60be196af34d250b9c6948f
leesw1347 <leesw1347@gmail.com>
2022-05-10 10:22:33 +0900

    57. foreach와 lambda 형식을 이용하여 List 출력하기

---
+++ java200example/src/kr/co/infopub/chapter/s057/ForeachLambdaLotto.java
@@ -0,0 +1,9 @@
+package kr.co.infopub.chapter.s057;
+
+import java.util.List;
+
+public class ForeachLambdaLotto {
+    public static void main(String[] args) {
+        List<Integer> mmlist
+    }
+}
