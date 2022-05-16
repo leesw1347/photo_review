2c7be1951bf11c3f34dd16afdef9623208d17f0d
leesw1347 <leesw1347@gmail.com>
2022-05-17 07:42:24 +0900

    70. StringBuilder로 문자열 수정, 변경, 추가하기  String과 비슷하지만 다른 특징을 가진 StringBuilder를 배워본다  StringBuilder는 동기화하지 않은 StringBuffer이다
     StringBuilder는 메모리 객체 주소가 할당할때마다 변경되기 때문에, 멀티 쓰레드 환경에서는 올바른 변수 활용법은 아니다

--- java200example/src/kr/co/infopub/chapter/s070/StringBuilderMain.java
+++ java200example/src/kr/co/infopub/chapter/s070/StringBuilderMain.java
@@ -1,4 +1,37 @@
 package kr.co.infopub.chapter.s070;
 
 public class StringBuilderMain {
+    public static void main(String[] args) {
+        //synchronized 안한 StringBuffer
+        StringBuilder sbu = new StringBuilder();
+        sbu.append("I")
+                .append(" go")
+                .append(" to school.");
+        print(sbu);
+
+//        System.out.println(sbu.substring(7, 11)); == sch
+        sbu.replace(7, 11, "");
+        print(sbu);
+
+        sbu.reverse();
+        print(sbu);
+
+        sbu.delete(1, 3); // sbu[1:3] 삭제
+        print(sbu);
+
+        String ss = sbu.substring(0); // sub[0:] 부터 데이터를 가져온다
+        print(new StringBuilder("=====\n" + new StringBuilder(ss)));
+        print(sbu);
+
+        String st = sbu.substring(0, 4); // sbu[0:4] 까지의 데이터를 가져온다
+        System.out.print("====\n");
+        System.out.println(st);
+        System.out.println(sbu);
+
+    }
+
+    public static void print(StringBuilder sb) {
+        System.out.print(sb.toString());
+        System.out.println();
+    }
 }
