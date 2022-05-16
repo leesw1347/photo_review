ee0faac85ff1c970fa05bf31f30512888624eee9
leesw1347 <leesw1347@gmail.com>
2022-05-16 23:10:09 +0900

    63. 수학연산에서 발생하는 예외 처리하기

---
+++ java200example/src/kr/co/infopub/chapter/s063/FinallyTryCatch.java
@@ -0,0 +1,9 @@
+package kr.co.infopub.chapter.s063;
+
+public class FinallyTryCatch {
+    public static void main(String[] args) {
+        try {
+            //수학 연산에서 발생하는 예외를 처리한다
+        }
+    }
+}
