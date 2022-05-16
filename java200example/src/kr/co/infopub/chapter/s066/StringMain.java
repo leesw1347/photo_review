3fc36fde09ee7ec560eead7f7b226bb6cb13eba5
leesw1347 <leesw1347@gmail.com>
2022-05-17 00:47:55 +0900

    66. String(문자열)을 이용해 도시 이름 비교하기

---
+++ java200example/src/kr/co/infopub/chapter/s066/StringMain.java
@@ -0,0 +1,9 @@
+package kr.co.infopub.chapter.s066;
+
+public class StringMain {
+    public static void main(String[] args) {
+        // String(문자열)을 이용해 도시 이름 비교하기
+        // String(문자열)이 동일한지 판별해보자
+
+    }
+}
