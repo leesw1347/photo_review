f42d1f26c5335fb2e1e34bb49e03b459a623cda0
leesw1347 <leesw1347@gmail.com>
2022-05-17 19:18:05 +0900

    2. System으 환경변수 확인하기

---
+++ java200example/src/kr/co/infopub/chapter/s072/SystemPropertyTest.java
@@ -0,0 +1,8 @@
+package kr.co.infopub.chapter.s072;
+
+public class SystemPropertyTest {
+    // System.getProperty()는 시스템 변수를, System.getenv()는 자바 환경변수를 반환한다
+    public static void main(String[] args) {
+        
+    }
+}
