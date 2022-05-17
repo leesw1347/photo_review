592fe00f62ed2f346b509ca02e5e8bb63e52c58a
leesw1347 <leesw1347@gmail.com>
2022-05-17 19:39:57 +0900

    73. 수학 관련 메서드 사용하기

--- java200example/src/kr/co/infopub/chapter/s073/MathTestMain.java
+++ java200example/src/kr/co/infopub/chapter/s073/MathTestMain.java
@@ -10,6 +10,13 @@ public class MathTestMain {
         System.out.println("Math.ceil(4.34) 올림: " + Math.ceil(4.34));
         System.out.println("Math.round(4.34) 반올림: " + Math.round(4.5));
         System.out.println("Math.floor(4.34) 버림: " + Math.floor(4.34));
-
+        System.out.println("Math.max(45,78): " + Math.max(45, 78)); // 45와 78 중에서 큰 값을 선택한다
+        System.out.println("Math.min(45,78): " + Math.min(45, 78)); // 45와 78 중에서 작은 값을 선택한다
+        System.out.println("Math.pow(2,4) : " + Math.pow(2, 4)); // 2**4를 의미한다 (pow는 제곱을 의미한다)
+        System.out.println("Math.exp(3): " + Math.exp(3));
+        System.out.println("Math.sqrt(10) " + Math.sqrt(10));
+        // Random
+        double dRan = Math.random(); // 0 <= dRan < 1 실수
+        System.out.println("Math.random() : " + dRan);
     }
 }
