018eb0f7d6d427813b70240254e3a9cd094dcc46
leesw1347 <leesw1347@gmail.com>
2022-05-17 23:35:41 +0900

    83. 빌보드 차트 정보를 빌보드 저장객체 (DTO)에 저장하기

---
+++ java200example/src/kr/co/infopub/chapter/s083/Billboard.java
@@ -0,0 +1,17 @@
+package kr.co.infopub.chapter.s083;
+
+import java.io.Serializable;
+
+public class Billboard implements Serializable {
+    private int rank;       // 순위
+    private String song;    // 곡명
+    private int lastweek;   // 전 주 순위
+    private String imagesrc;    // 가수 사진
+    private String artistsrc;   // 가수 정보
+    private String artist;      // 가수 이름
+    
+
+    public static void main(String[] args) {
+
+    }
+}
