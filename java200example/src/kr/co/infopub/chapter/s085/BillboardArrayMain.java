fd264f518231a469d470bad59aa6cba937b6e362
leesw1347 <leesw1347@gmail.com>
2022-05-18 13:37:24 +0900

    85. 빌보드 차트를 리스트에 저장하고 출력하기

---
+++ java200example/src/kr/co/infopub/chapter/s085/BillboardArrayMain.java
@@ -0,0 +1,10 @@
+package kr.co.infopub.chapter.s085;
+
+// 빌보드 차트를 리스트에 저장하고 출력하기
+public class BillboardArrayMain {
+    public static void main(String[] args) {
+        // 빌보드 차트 정보를 객체에 저장한 다음, 객체를 리스트에 저장하고 출력해보자
+        // 리스트는 향상된 배열로 저장할 때 add, 사용할 때는 get을 사용한다
+        
+    }
+}
