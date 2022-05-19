eed823f99f8f84ad4f13ff4dc17ff8a5e79641f8
leesw1347 <leesw1347@gmail.com>
2022-05-19 19:08:41 +0900

    114. 홀수 마방진 만들기     홀수 마방진을 만들어보자     홀수 마방진을 만드는 방법을 따른다

---
+++ java200example/src/kr/co/infopub/chapter/s114/OddMagicSquare.java
@@ -0,0 +1,11 @@
+package kr.co.infopub.chapter.s114;
+
+public class OddMagicSquare {
+    private int[][] magic; // 2차원 배열 선언
+    private int n; // 마방진 크기 n X n
+
+    public int[][] getMagic() {
+        return magic;
+    }
+
+}
