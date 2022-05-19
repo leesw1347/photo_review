5e518b652f60bd14c35453207bdc1581640652d2
leesw1347 <leesw1347@gmail.com>
2022-05-19 17:49:33 +0900

    110. 카드 게임 (섯다) 족보 만들기     카드 게임을 위한 순위(족보)를 만들어본다     두 카드의 값이 같으면, 한 카드에 100을 곱하고, 다르면 두 카드값의 합에 대한 일의 자리에 10을 곱한다

---
+++ java200example/src/kr/co/infopub/chapter/s110/CardRuleMain.java
@@ -0,0 +1,13 @@
+package kr.co.infopub.chapter.s110;
+
+import kr.co.infopub.chapter.s109.CardCase;
+
+public class CardRuleMain {
+    public static void main(String[] args) {
+        CardCase cc = new CardCase(); // 카드 저장
+        cc.make(); // 카드 만들고
+        CardRule rule = new CardRule(); // 족보
+
+
+    }
+}
