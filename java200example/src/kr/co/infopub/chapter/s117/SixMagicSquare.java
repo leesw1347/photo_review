c5056945b0ff54b72669e2d8f52616efe68e917a
leesw1347 <leesw1347@gmail.com>
2022-05-21 01:03:00 +0900

    117. 6 마방진 만들기

---
+++ java200example/src/kr/co/infopub/chapter/s117/SixMagicSquare.java
@@ -0,0 +1,15 @@
+package kr.co.infopub.chapter.s117;
+
+public class SixMagicSquare {
+
+    private int[][] magic;
+    private int n;
+
+    public int[][] getMagic() {
+        return magic;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
