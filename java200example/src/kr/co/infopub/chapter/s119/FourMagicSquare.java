6ee48314c0f15895d9870e6f54906a2cd38802bc
leesw1347 <leesw1347@gmail.com>
2022-05-22 20:49:23 +0900

    119. 추상 클래스, 인터페이스로 마방진 만들기

---
+++ java200example/src/kr/co/infopub/chapter/s119/FourMagicSquare.java
@@ -0,0 +1,24 @@
+package kr.co.infopub.chapter.s119;
+
+public class FourMagicSquare extends MagicSquare {
+
+    public FourMagicSquare(int n) {
+        super(n);
+    }
+
+    public FourMagicSquare() {
+        this(4); // 또 다른 자신의 생성자를 호출한다
+    }
+
+    public void make() {
+//        super.make();
+        makeA();
+        makeR();
+    }
+
+    public void makeR() {
+    }
+
+    public void makeA() {
+    }
+}
---
+++ java200example/src/kr/co/infopub/chapter/s119/MagicSquareTest.java
@@ -0,0 +1,14 @@
+package kr.co.infopub.chapter.s119;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MagicSquareTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
