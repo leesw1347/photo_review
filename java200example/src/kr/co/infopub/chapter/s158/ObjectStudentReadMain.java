e856608638660f67f575e8a22da6693da8c4e231
leesw1347 <leesw1347@gmail.com>
2022-05-23 02:46:34 +0900

    158. 객체 직렬화, 제네릭을 이용하여 객체 저장하기

---
+++ java200example/src/kr/co/infopub/chapter/s158/ObjectStudentReadMain.java
@@ -0,0 +1,8 @@
+package kr.co.infopub.chapter.s158;
+
+public class ObjectStudentReadMain {
+    public static void main(String[] args) {
+        ObjectStudentRWG<Student> objectStudentRWG = new ObjectStudentRWG<>();
+        objectStudentRWG.read("Student.Object");
+    }
+}
