cc59774fd21446ad5d235d7f20efee1d37db7028
이선우 (Lee Seon Woo) <leesw1347@gmail.com>
2022-07-13 04:58:43 +0900

    Delete android-package/find-java-learn-algorithm-datastructed/src directory

--- android-package/find-java-learn-algorithm-datastructed/src/Chapter3/Part3/Sub5/MyLinkedListUnitTest.java
+++
@@ -1,8 +0,0 @@
-package Chapter3.Part3.Sub5;
-
-public class MyLinkedListUnitTest {
-    public static void main(String[] args) {
-        MyLinkedList myLinkedList = new MyLinkedList();
-        System.out.println(myLinkedList + " 객체 선언에 성공하였습니다");
-    }
-}
